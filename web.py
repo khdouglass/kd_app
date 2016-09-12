bf2a177528984413f1f431b5c4ffe0c9b9155d20
Kathryn Douglass <kd@Kathryns-MacBook-Air.local>
2016-09-11 21:36:43 -0700

    Deleted forecast info

--- web.py
+++ web.py
@@ -17,14 +17,6 @@ def index():
 		businesses = yelp_api.search_businesses(address)
 	return render_template('index.html', businesses=businesses)
 
-def index():
-	address = request.values.get('address')
-	forecast = None
-	#will only pass through function if there is an address
-	if address:
-		forecast = weather.get_weather(address)
-	return render_template('index.html', forecast=forecast)
-
 
 #necessary to get application to run 
 if __name__ == "__main__":
--- yelp_api.py
+++ yelp_api.py
@@ -9,7 +9,7 @@ load_dotenv(find_dotenv())
 
 def search_businesses(address):
 	auth = Oauth1Authenticator(
-    	consumer_key=os.environ['CONSUMER_KEY'],
+   		consumer_key=os.environ['CONSUMER_KEY'],
     	consumer_secret=os.environ['CONSUMER_SECRET'],
     	token=os.environ["TOKEN"],
     	token_secret=os.environ["TOKEN_SECRET"]
